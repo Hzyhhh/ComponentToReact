a03280e1d11b019d46f0172cb3792fc9caa0c0d1
Hzyhhh <799309084@qq.com>
2020-10-22 23:03:14 +0800

    prettier code

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import React, { FC } from 'react';
-import './App.css';
-import Form from './container/Form'
+import React, { FC } from "react";
+import "./App.css";
+import Form from "./container/Form";
 import moment from "moment";
-import 'antd/dist/antd.css'
+import "antd/dist/antd.css";
 // 推荐在入口文件全局设置 locale
 import "moment/locale/zh-cn";
 moment.locale("zh-cn");
 
-const App:FC =()=> {
+const App: FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-      <Form />
+        <Form />
       </header>
     </div>
   );
-}
+};
 
 export default App;
